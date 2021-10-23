fe965feefae7fd62337f3b1295df675f46f631ce
Vitaliy Morozov <vitalick.morozov2011@mail.ru>
2021-10-23 18:43:30 +0300

    first project

--- Test/src/com/java4ushka/java/Soska.java
+++ Test/src/com/java4ushka/java/Soska.java
@@ -1,5 +1,14 @@
 package com.java4ushka.java;
-public class {
+import java.util.Scanner;
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner("Люблю тебя, Петра творенье,\n" +
+                "Люблю твой строгий, стройный вид,\n" +
+                "Невы державное теченье,\n" +
+                "Береговой ее гранит");
+        String s = scanner.nextLine();
+        System.out.println(s);
+    }
 }
-
-
